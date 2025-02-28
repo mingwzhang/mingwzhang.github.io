d32e902c0d64875f077cbf35684a189aea82f278
Mingwei Zhang <81440878+mingwzhang@users.noreply.github.com>
2025-02-27 19:16:51 -0500

    Re-initialize correct folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar"; // We will create this next
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to My Portfolio</h1>
+      <p>Hi, I'm Mingwei Zhang, a software and game developer.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <h2>My Portfolio</h2>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
